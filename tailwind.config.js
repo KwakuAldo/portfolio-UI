aa2ac2a28b88b16a894eaf041bfb072ef5fa8ad1
KwakuAldo <x.pac20032001@gmail.com>
2024-03-31 00:24:03 +0000

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1200px",
+    },
+    extend: {
+      colors: {
+        darkBlue: 'hsla(234, 30%, 18%, 1)',
+        pinkish: 'hsla(0, 100%, 70%, 1)',
+        veryLightBlue: 'hsla(194, 57%, 95%, 1)',
+        light: 'hsla(210, 14%, 58%, 1)'
+      },
+    },
+  },
+  plugins: [],
+}
